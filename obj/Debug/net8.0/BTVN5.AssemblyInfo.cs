b8f672f09f2b8f8174a39f9448459714b53d7f15
VanNghia <nghiadauvan@gmail.com>
2024-03-14 15:00:28 +0700

    update trang chu

--- obj/Debug/net8.0/BTVN5.AssemblyInfo.cs
+++ obj/Debug/net8.0/BTVN5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTVN5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe49cf01584b0161b5518d53ec4a6a0be7c52aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTVN5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTVN5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
